14f1e4617885f10a9d3d359c457072cd9500fd87
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-20 15:52:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,34 +1,43 @@
 class Animal {
-  constructor(species) {
-    this._species = species;
-  }
+    constructor(species) {
+        this._species = species;
+    }
 
-  get species() {
-    return this._species;
-  }
+    get species() {
+        return this._species;
+    }
 
-  makeSound(sound) {
-    console.log(sound);
-  }
+    makeSound() {
+        // This method will be overridden in the subclasses
+    }
 }
 
 class Cat extends Animal {
-  purr() {
-    this.makeSound("purr");
-  }
+    purr() {
+        console.log("purr");
+    }
+
+    makeSound() {
+        this.purr();  // A cat's sound is purring
+    }
 }
 
 class Dog extends Animal {
-  bark() {
-    this.makeSound("woof");
-  }
+    bark() {
+        console.log("woof");
+    }
+
+    makeSound() {
+        this.bark();  // A dog's sound is barking
+    }
 }
 
-// Usage example
-const cat = new Cat("Domestic Cat");
-const dog = new Dog("Golden Retriever");
+// Example usage:
+const myCat = new Cat("Felis catus");
+const myDog = new Dog("Canis lupus familiaris");
 
-console.log(cat.species); // Output: Domestic Cat
-cat.purr(); // Output: purr
-console.log(dog.species); // Output: Golden Retriever
-dog.bark(); 
+console.log(`My cat belongs to the species: ${myCat.species}`);
+myCat.makeSound();
+
+console.log(`My dog belongs to the species: ${myDog.species}`);
+myDog.makeSound();
