701126f13e0337073d7b72f9dd0a65fda2cf92a5
prithvirajbarad <112259618+prithvirajbarad@users.noreply.github.com>
2023-10-22 18:57:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,25 @@
 class Animal {
-    constructor(species) {
-        this._species = species;
-    }
+  constructor(species) {
+    this._species = species;
+  }
 
-    get species() {
-        return this._species;
-    }
+  get species() {
+    return this._species;
+  }
 
-    makeSound() {
-        // This method will be overridden in the subclasses
-    }
+  makeSound() {
+    console.log("Animal sound");
+  }
 }
 
 class Cat extends Animal {
-    purr() {
-        console.log("purr");
-    }
-
-    makeSound() {
-        this.purr();  // A cat's sound is purring
-    }
+  purr() {
+    console.log("purr");
+  }
 }
 
 class Dog extends Animal {
-    bark() {
-        console.log("woof");
-    }
-
-    makeSound() {
-        this.bark();  // A dog's sound is barking
-    }
+  bark() {
+    console.log("woof");
+  }
 }
-
-// Example usage:
-const myCat = new Cat("Felis catus");
-const myDog = new Dog("Canis lupus familiaris");
-
-console.log(`My cat belongs to the species: ${myCat.species}`);
-myCat.makeSound();
-
-console.log(`My dog belongs to the species: ${myDog.species}`);
-myDog.makeSound();
